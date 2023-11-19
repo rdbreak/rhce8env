dd5f12ba131b583dc2b3a8dfa37a76e58b1c16e0
victorbrca <victorbrca@users.noreply.github.com>
2023-11-19 16:26:43 -0500

    Moving playbooks into a role

---
+++ roles/build-nodes/files/welcome-repo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+echo -e '
+#           _          _           _         _
+#          /\ \       /\ \        /\ \      /\ \
+#         /  \ \     /  \ \      /  \ \    /  \ \
+#        / /\ \ \   / /\ \ \    / /\ \ \  / /\ \ \
+#       / / /\ \_\ / / /\ \_\  / / /\ \_\/ / /\ \ \
+#      / / /_/ / // /_/_ \/_/ / / /_/ / / / /  \ \_\
+#     / / /__\/ // /____/\   / / /__\/ / / /   / / /
+#    / / /_____// /\____\/  / / /_____/ / /   / / /
+#   / / /\ \ \ / / /______ / / /     / / /___/ / /
+#  / / /  \ \ / / /_______/ / /     / / /____\/ /
+#  \/_/    \_\\/__________\/_/      \/_________/
+'"#
+# Why are you here? You should be working on the system hosts...
+# Anyway, try not to reboot me or bad stuff can happen.
+#
+# You are logged into \"`hostname`\" as the \"`whoami`\" account.
+# This system is running `cat /etc/redhat-release`
+#
+"
